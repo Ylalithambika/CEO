f709908d7992db47b09f656d5699b680fb738ecb
Lalli <lalithambikay1@gmail.com>
2021-04-13 13:48:05 +0530

    first commit

---
+++ MavenDemo/src/main/java/com/hcl/MavenDemo/App.java
@@ -0,0 +1,14 @@
+package com.hcl.MavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "iam lalitha" );
+        System.out.println("hello");
+    }
+}
